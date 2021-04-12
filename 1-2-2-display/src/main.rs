cd75a933aa258d334300ebe037f92c1a213680f3
Collin Smith <collinsmith70@gmail.com>
2021-04-12 01:51:18 -0700

    Created crate for 1-2-2-display

---
+++ 1-2-2-display/src/main.rs
@@ -0,0 +1,119 @@
+mod basic {
+    // Import (via `use`) the `fmt` module to make it available.
+    use std::fmt;
+
+    // Define a structure for which `fmt::Display` will be implemented. This is
+    // a tuple struct named `Structure` that contains an `i32`.
+    #[derive(Debug)]
+    struct Structure(i32);
+
+    // To use the `{}` marker, the trait `fmt::Display` must be implemented
+    // manually for the type.
+    impl fmt::Display for Structure {
+        // This trait requires `fmt` with this exact signature.
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Write strictly the first element into the supplied output
+            // stream: `f`. Returns `fmt::Result` which indicates whether the
+            // operation succeeded or failed. Note that `write!` uses syntax
+            // which is very similar to `println!`.
+            write!(f, "{}", self.0)
+        }
+    }
+
+    pub fn main() {
+        let structure = Structure(1234);
+        println!("Display: {}", structure);
+        println!("Debug: {:?}", structure);
+    }
+}
+
+mod advanced {
+    use std::fmt; // Import `fmt`
+
+    // A structure holding two numbers. `Debug` will be derived so the results
+    // can be contrasted with `Display`.
+    #[derive(Debug)]
+    struct MinMax(i64, i64);
+
+    // Implement `Display` for `MinMax`.
+    impl fmt::Display for MinMax {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Use `self.number` to refer to each positional data point.
+            write!(f, "({}, {})", self.0, self.1)
+        }
+    }
+
+    // Define a structure where the fields are nameable for comparison.
+    #[derive(Debug)]
+    struct Point2D {
+        x: f64,
+        y: f64,
+    }
+
+    // Similarly, implement `Display` for `Point2D`
+    impl fmt::Display for Point2D {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Customize so only `x` and `y` are denoted.
+            write!(f, "x: {}, y: {}", self.x, self.y)
+        }
+    }
+
+    pub fn main() {
+        let minmax = MinMax(0, 14);
+
+        println!("Compare structures:");
+        println!("Display: {}", minmax);
+        println!("Debug: {:?}", minmax);
+
+        let big_range = MinMax(-300, 300);
+        let small_range = MinMax(-3, 3);
+
+        println!("The big range is {big} and the small is {small}",
+                small = small_range,
+                big = big_range,
+                );
+
+        let point = Point2D { x: 3.3, y: 7.2 };
+
+        println!("Compare points:");
+        println!("Display: {}", point);
+        println!("Debug: {:?}", point);
+
+        // Error. Both `Debug` and `Display` were implemented, but `{:b}`
+        // requires `fmt::Binary` to be implemented. This will not work.
+        // println!("What does Point2D look like in binary: {:b}?", point);
+    }
+}
+
+mod list {
+    use std::fmt;
+
+    struct List(Vec<i32>);
+
+    impl fmt::Display for List {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            let vec = &self.0;
+            write!(f, "[")?;
+            for (count, v) in vec.iter().enumerate() {
+                if count != 0 { write!(f, ", ")?; }
+                write!(f, "{}", v)?;
+            }
+
+            write!(f, "]")
+        }
+    }
+
+    pub fn main() {
+        let v = List(vec![1, 2, 3]);
+        println!("{}", v);
+    }
+}
+
+fn main() {
+    println!("basic---------------------------");
+    basic::main();
+    println!("advanced------------------------");
+    advanced::main();
+    println!("list----------------------------");
+    list::main();
+}
